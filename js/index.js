02eb99f27a3a5bb5681aa9b6eb25619f30cd9c00
Chi-Hsuan, Huang <chihsuan.tw@gmail.com>
2017-04-05 21:04:10 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 (function(window) {
+
   var eastAsiaCountries = ['China', 'Taiwan', 'Japan', 'Vietnam',
     'North Korea', 'South Korea', 'Philippines', 'Thailand', 'Malaysia',
     'Indonesia', 'Cambodia', 'Laos', 'Myanmar', 'Singapore', 'Brunei',
@@ -11,15 +12,22 @@
     .attr("width", width)
     .attr("height", height);
 
+  var scale = 500;
+  var isMobile = width < 600;
+
+  if (isMobile)  {
+    scale = 300;
+  }
+
   d3.json("data/countries.geo.topo.json", function(error, mapData) {
     if (error) return console.error(error);
 
-    var center = [122, 23];
+    var center = [110.5, 23];
 
     var projection = d3.geo.mercator()
-      .scale(500)
+      .scale(scale)
       .center(center)
-      .translate([width / 2, height / 2]);
+      .translate([width / 2 + 10, height / 2 + 50]);
 
     var path = d3.geo.path()
       .projection(projection);
@@ -40,7 +48,7 @@
       })
       .attr('fill', function(d) {
         if(d.properties.name === 'Vietnam') {
-          return 'red';
+          return 'rgba(218, 55, 47, 0.9)';
         } 
       });
 
@@ -56,12 +64,113 @@
     svg.append('text')
       .text('East Asia')
       .attr('class', 'map-title')
-      .attr("y", height/2)
-      .attr("x", width/3*2);
+      .attr("y", function() {
+        if (isMobile) {
+          return '150px';
+        }
+        return height/2;
+      })
+      .attr("x", function() {
+        if (isMobile) {
+          return width/2;
+        }
+        return width/5*4;
+      })
+      .attr("text-anchor", "middle");
+
+
+    var features = topojson.feature(mapData, mapData.objects['countries.geo']).features;
+    features = features.filter(function(d) {
+      return d.properties.name === 'Vietnam';
+    });
+
+    var centroids = features.map(function (feature){
+      return {
+        name: feature.properties.name,
+        center: path.centroid(feature),
+      }
+    });
+
+    svg.selectAll(".place-label")
+      .data(centroids)
+    .enter().append("text")
+      .attr("class", "place-label")
+      .text(function(d) { return d.name; })
+      .attr("x", function (d){ return d.center[0]; })
+      .attr("y", function (d){ return d.center[1]; });
 
   });
 
+  var root = { children: [
+    {value: 4}, {value: 2}, 
+    {value: 10}, {value: 5},
+    {value: 8}, {value: 10},    
+  ]};
+
+  var bankTreemap = d3.layout.treemap().size([width,400]);
+  var nodes = bankTreemap.nodes(root);
+  nodes = nodes.slice(1, nodes.length);
+
+  var bankSvg = d3.select('#bank-treemap')
+    .append('svg')
+    .attr("width", width)
+    .attr("height", 400);
+
+  bankSvg.selectAll("rect")
+    .data(nodes)
+    .enter().append("rect")
+    .attr({
+      x: function(d) { return d.x; },
+      y: function(d) { return d.y; },
+      width: function(d) { return d.dx; },
+      height: function(d) { return d.dy; },
+      class: 'bank-treemap-rect'
+    })
+
+  bankSvg.selectAll("text")
+    .data(nodes)
+    .enter().append("text")
+    .attr({
+      x: function(d) { return d.x + d.dx/4; },
+      y: function(d) { return d.y + d.dy/3; },
+      "text-anchor": "middle",
+      "dominant-baseline": "central"
+    })
+    .text(function(d) { return 'Bank' })
+    .style('font-size', '16px');
 
 
+
+  var projTreemap = d3.layout.treemap().size([width,400]);
+  var nodes = bankTreemap.nodes(root);
+  nodes = nodes.slice(1, nodes.length);
+
+  var projSvg = d3.select('#proj-treemap')
+    .append('svg')
+    .attr("width", width)
+    .attr("height", 400);
+
+  projSvg.selectAll("rect")
+    .data(nodes)
+    .enter().append("rect")
+    .attr({
+      x: function(d) { return d.x; },
+      y: function(d) { return d.y; },
+      width: function(d) { return d.dx; },
+      height: function(d) { return d.dy; },
+      class: 'proj-treemap-rect'
+    })
+
+  projSvg.selectAll("text")
+    .data(nodes)
+    .enter().append("text")
+    .attr({
+      x: function(d) { return d.x + d.dx/4; },
+      y: function(d) { return d.y + d.dy/3; },
+      "text-anchor": "middle",
+      "dominant-baseline": "central"
+    })
+    .text(function(d) { return 'Project' })
+    .style('font-size', '16px');
 
 })(window);
